2bbdc7eeb24721f61c76a6f5183cc4a371f8ce01
nathanbernardo <nathan.c.bernardo@gmail.com>
2026-02-07 13:42:49 -0800

    feat: add additional plugins

--- lua/nathan/plugins/snacks.lua
+++ lua/nathan/plugins/snacks.lua
@@ -1,85 +1,23 @@
 return {
-  "folke/snacks.nvim",
-  priority = 1000,
-  lazy = false,
-  ---@types snacks.Config
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-    -- bigfile = { enabled = true },
-    dashboard = {
-      enabled = true,
-      sections = {
-        { section = "header" },
-        {
-          pane = 2,
-          section = "terminal",
-          height = 5,
-          padding = 1,
-        },
-        { section = "keys", gap = 1, padding = 1 },
-        { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-        { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
-        {
-          pane = 2,
-          icon = " ",
-          title = "Git Status",
-          section = "terminal",
-          enabled = function()
-            return Snacks.git.get_root() ~= nil
-          end,
-          cmd = "git status --short --branch --renames",
-          height = 5,
-          padding = 1,
-          ttl = 5 * 60,
-          indent = 3,
-        },
-        { section = "startup" },
-      }
-    },
-    -- explorer = { enabled = true},
-    ---@class snacks.input.Config
-    indent = { enabled = true },
-    lazygit = { enabled = true },
-    input = {
-      enabled = true,
-      height = 10,
-    },
-    picker = { enabled = true },
-    notifier = { enabled = true },
-    -- quickfile = { enabled = true },
-    -- scope = { enabled = true },
-    -- scroll = { enabled = true },
-    -- statuscolumn = { enabled = true },
-    -- words = { enabled = true },
-  },
-  keys = {
-    -- git
-    { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
-    { "<leader>gb", function() Snacks.picker.git_branches() end, desc = "Git Branches" },
-    { "<leader>gl", function() Snacks.picker.git_log() end, desc = "Git Log" },
-    { "<leader>gL", function() Snacks.picker.git_log_line() end, desc = "Git Log Line" },
-    { "<leader>gs", function() Snacks.picker.git_status() end, desc = "Git Status" },
-    { "<leader>gS", function() Snacks.picker.git_stash() end, desc = "Git Stash" },
-    { "<leader>gd", function() Snacks.picker.git_diff() end, desc = "Git Diff (Hunks)" },
-    { "<leader>gf", function() Snacks.picker.git_log_file() end, desc = "Git Log File" },
-    -- notifier
-    { "<leader>n",  function() Snacks.notifier.show_history() end, desc = "Notification History" },
-  },
-  init = function()
-    vim.api.nvim_create_autocmd("User", {
-      pattern = "VeryLazy",
-      callback = function()
-        _G.dd = function(...)
-          Snacks.debug.inspect()
-        end
-        _G.bt = function()
-          Snacks.debug.backtrace()
-        end
-        vim.print = _G.dd
-        
-      end,
-    })
-  end,
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	---@type snacks.Config
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+		bigfile = { enabled = false },
+		dashboard = { enabled = false },
+		explorer = { enabled = false },
+		indent = { enabled = false },
+		input = { enabled = true },
+		picker = { enabled = false },
+		notifier = { enabled = false },
+		quickfile = { enabled = false },
+		scope = { enabled = false },
+		scroll = { enabled = false },
+		statuscolumn = { enabled = false },
+		words = { enabled = false },
+	},
 }
---
+++ lua/nathan/plugins/zen.lua
@@ -0,0 +1,11 @@
+return {
+	"folke/zen-mode.nvim",
+	keys = {
+		{ "<leader>z", "<cmd>ZenMode<CR>", desc = "Toggle zen mode" },
+	},
+	opts = {
+		window = {
+			width = 150,
+		},
+	},
+}
